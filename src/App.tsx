b767640c89e8133ac51be08193f025f6fbe765e3
DevFC-M <felipech3344@gmail.com>
2022-09-14 22:18:21 -0300

    Configuração do tailwind para estilização do projetos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import './styles/main.css'
+
 function App() {
-  return <h1>Hello Nlw</h1>
+  return (
+    <div></div>
+  )
 }
 
 export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx', // Caminho onde terão estilização Tailwind 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
