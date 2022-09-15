87922f3659a20b7c028b75308c577d5a45074986
DevFC-M <felipech3344@gmail.com>
2022-09-15 00:10:35 -0300

    Criação da sessão de publicação de anuncio na landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/main.css';
 
 import logoImg from './assets/logo_nlw.svg'
+import { MagnifyingGlassPlus }from 'phosphor-react'
 
 function App() {
   return (
@@ -11,48 +12,82 @@ function App() {
 
       <div className="grid grid-cols-6 gap-6 mt-16">
         <a href="" className="relative rounded-lg overflow-hidden">
+
           <img src="/game-1.png" alt="" />
+
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font bold text-white block">League of Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anuncios</span>
           </div>
+
         </a>
         <a href="" className="relative rounded-lg overflow-hidden">
+
           <img src="/game-2.png" alt="" />
+
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font bold text-white block">League of Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anuncios</span>
           </div>
+
         </a>
         <a href="" className="relative rounded-lg overflow-hidden">
+
           <img src="/game-3.png" alt="" />
+
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font bold text-white block">League of Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anuncios</span>
           </div>
+
         </a>
         <a href="" className="relative rounded-lg overflow-hidden">
+
           <img src="/game-4.png" alt="" />
+
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font bold text-white block">League of Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anuncios</span>
           </div>
+
         </a>
         <a href="" className="relative rounded-lg overflow-hidden">
+
           <img src="/game-5.png" alt="" />
+
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font bold text-white block">League of Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anuncios</span>
           </div>
+
         </a>
         <a href="" className="relative rounded-lg overflow-hidden">
+
           <img src="/game-6.png" alt="" />
+
           <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
             <strong className="font bold text-white block">League of Legends</strong>
             <span className="text-zinc-300 text-sm block">4 anuncios</span>
           </div>
+
         </a>
       </div>
+      <div className="pt-1 bg-duo-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] py-6 px-8 flex justify-between items-center">
+          <div>
+            <strong className="block text-2xl text-white font-black ml-8">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 ml-8 block">Publique um anúncio para encontrar novos players!</span> 
+          </div>
+
+          <button className="py-3 px-4 hover:bg-violet-600 bg-violet-500 rounded text-white font-medium flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,13 @@ module.exports = {
     './index.html', // Caminho onde terão estilização Tailwind 
   ], 
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')", 
-        'duo-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 33.57%)',
+        'duo-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       },
     },
