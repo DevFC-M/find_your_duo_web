5bb0d0feee9b2eaec20aeb1d0058d8d17af7610f
DevFC-M <felipech3344@gmail.com>
2022-09-14 23:14:01 -0300

    Criação da landing page commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import './styles/main.css'
+import './styles/main.css';
+
+import logoImg from './assets/logo_nlw.svg'
 
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-500">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" className=""/>
 
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="text-transparent bg-duo-gradient bg-clip-text">duo</span> está aqui.</h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anuncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
