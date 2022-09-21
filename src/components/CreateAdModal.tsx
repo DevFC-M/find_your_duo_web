bb980c19879f433af09cb2fe4274296c77c6d7a2
DevFC-M <felipech3344@gmail.com>
2022-09-20 22:54:50 -0300

    Criação da estilização da sessão de dias que costuma jogar com radix ui togle

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -5,6 +5,8 @@ import * as Checkbox from '@radix-ui/react-checkbox'
 import { useEffect, useState } from 'react';
 import { SelectGame } from './Forms/SelectGame';
 
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+
 interface Game {
     id: string;
     title: string;
@@ -12,125 +14,138 @@ interface Game {
 
 export function CreateAdModal() {
 
-    const [games, setGames] = useState <Game[]>([]);
+  const [games, setGames] = useState <Game[]>([]);
+  const [weekDays,  setWeekDays] = useState<string[]>([])
 
-    useEffect(() => {
-        fetch('http://localhost:19001/games')
-          .then(response => response.json())
-          .then(data => {
-            setGames(data);
-          })
-      }, []);
+  useEffect(() => {
+      fetch('http://localhost:19001/games')
+        .then(response => response.json())
+        .then(data => {
+          setGames(data);
+        })
+    }, []);
 
-    return (
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/60 inset-0 fixed"/>
+  return (
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/60 inset-0 fixed"/>
 
-          <Dialog.Content className="fixed bg-[#2a2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg">
-            <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
-            <form className="mt-8 flex flex-col gap-4">
+        <Dialog.Content className="fixed bg-[#2a2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg">
+          <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
+          <form className="mt-8 flex flex-col gap-4">
 
+            <div className="flex flex-col gap-2">
+              <label htmlFor="game" className="font-semibold">Qual o game?</label>
+              <SelectGame games={games}/>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <label htmlFor="name">Seu nome (ou nickname)</label>
+              <Input id="name" placeholder="Como te chamam dentro do game?" />
+            </div>
+
+            <div className="grid grid-cols-2 gap-6">
               <div className="flex flex-col gap-2">
-                <label htmlFor="game" className="font-semibold">Qual o game?</label>
-                <SelectGame games={games}/>
+                <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
+                <Input id="yearsPlaying" placeholder="Tudo bem ser ZERO" />
               </div>
-
               <div className="flex flex-col gap-2">
-                <label htmlFor="name">Seu nome (ou nickname)</label>
-                <Input id="name" placeholder="Como te chamam dentro do game?" />
+                <label htmlFor="discord">Qual seu discord?</label>
+                <Input id="discord" placeholder="Usuario#0000" />
               </div>
+            </div>
 
-              <div className="grid grid-cols-2 gap-6">
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
-                  <Input id="yearsPlaying" placeholder="Tudo bem ser ZERO" />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label htmlFor="discord">Qual seu discord?</label>
-                  <Input id="discord" placeholder="Usuario#0000" />
-                </div>
-              </div>
-
-              <div className="flex gap-6">
-                <div className= "flex flex-col gap-2">
-                  <label htmlFor="weekDays">Quando costuma jogar?</label>
-                  <div className="grid grid-cols-4 gap-2">
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Domingo"
-                      >
-                        D
-                      </button>
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Segunda"
-                      >
-                        S
-                      </button>
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Terça">
-                        T
-                      </button>
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Quarta">
-                        Q
-                      </button>
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Quinta">
-                        Q
-                      </button>
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Sexta">
-                        S
-                      </button>
-                    <button 
-                      className="w-8 h-8 rounded bg-zinc-900" 
-                      title="Sexta">
-                        S
-                      </button>
-                  </div>
-                </div>
-                <div className= "flex flex-col gap-2 flex-1">
-                  <label htmlFor="hoursStart">Qual horário do dia</label>
-                  <div className="grid grid-cols-2 gap-2">
-                    <Input type="time" id="hoursStart" placeholder="De"/>
-                    <Input type="time" id="hoursEnd" placeholder="Até"/>
-                  </div>
-                </div>
-              </div>
-              
-              <div className="mt-2 flex gap-2 text-sm items-center ">
-                <Checkbox.Root className="w-6 h-6 rounded bg-zinc-900 p-1"> 
-                    <Checkbox.Indicator>
-                        <Check 
-                            className="w-4 h-4 text-emerald-400"
-                        />
-                    </Checkbox.Indicator>
-                </Checkbox.Root>
-                Costumo me conectar no chat de voz
-              </div>
-
-              <footer className="mt-4 flex justify-end gap-4">
-                <Dialog.Close 
-                  type="button" 
-                  className="bg-zinc-500 px-5 h-12 rounded-md font-semibold"
+            <div className="flex gap-6">
+              <div className= "flex flex-col gap-2">
+                <label htmlFor="weekDays">Quando costuma jogar?</label>
+                <ToggleGroup.Root 
+                  type="multiple" 
+                  className="grid grid-cols-4 gap-2"
+                  value={weekDays}
+                  onValueChange={setWeekDays}
                 >
-                    Cancelar
-                </Dialog.Close>
-                <button
-                   type="submit"
-                   className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
-                >
-                  <GameController size={24}/>
-                  Encontrar duo
-                </button>
-              </footer>
-            </form>
-            </Dialog.Content>
-        </Dialog.Portal>
-    )
+                  <ToggleGroup.Item 
+                    value="0"
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('0') ? 'bg-violet-500' : ''}`} 
+                    title="Domingo"
+                    >
+                      D
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="1"
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('1') ? 'bg-violet-500' : ''}`} 
+                    title="Segunda"
+                    >
+                      S
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="2"
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('2') ? 'bg-violet-500' : ''}`} 
+                    title="Terça">
+                      T
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="3"
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('3') ? 'bg-violet-500' : ''}`} 
+                    title="Quarta">
+                      Q
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="4"
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('4') ? 'bg-violet-500' : ''}`} 
+                    title="Quinta">
+                      Q
+                    </ToggleGroup.Item>
+                  <ToggleGroup.Item
+                    value="5" 
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('5') ? 'bg-violet-500' : ''}`} 
+                    title="Sexta">
+                      S
+                    </ToggleGroup.Item>
+                  <ToggleGroup.Item 
+                    value="6"
+                    className={`w-8 h-8 rounded bg-zinc-900 ${weekDays.includes('6') ? 'bg-violet-500' : ''}`} 
+                    title="Sexta">
+                      S
+                  </ToggleGroup.Item>
+                </ToggleGroup.Root>
+              </div>
+              <div className= "flex flex-col gap-2 flex-1">
+                <label htmlFor="hoursStart">Qual horário do dia</label>
+                <div className="grid grid-cols-2 gap-2">
+                  <Input type="time" id="hoursStart" placeholder="De"/>
+                  <Input type="time" id="hoursEnd" placeholder="Até"/>
+                </div>
+              </div>
+            </div>
+            
+            <div className="mt-2 flex gap-2 text-sm items-center ">
+              <Checkbox.Root className="w-6 h-6 rounded bg-zinc-900 p-1"> 
+                  <Checkbox.Indicator>
+                      <Check 
+                          className="w-4 h-4 text-emerald-400"
+                      />
+                  </Checkbox.Indicator>
+              </Checkbox.Root>
+              Costumo me conectar no chat de voz
+            </div>
+
+            <footer className="mt-4 flex justify-end gap-4">
+              <Dialog.Close 
+                type="button" 
+                className="bg-zinc-500 px-5 h-12 rounded-md font-semibold"
+              >
+                  Cancelar
+              </Dialog.Close>
+              <button
+                  type="submit"
+                  className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
+              >
+                <GameController size={24}/>
+                Encontrar duo
+              </button>
+            </footer>
+          </form>
+        </Dialog.Content>
+      </Dialog.Portal>
+  )
 }
--- src/components/Forms/SelectGame.tsx
+++ src/components/Forms/SelectGame.tsx
@@ -1,51 +1,57 @@
 import * as Select from '@radix-ui/react-select';
 import { CaretDown, Check } from 'phosphor-react';
+import { useState } from 'react';
 
 interface SelectGameProps {
     games: any;
 }
 
 export function SelectGame(props: SelectGameProps) {
-    return(
-        <Select.Root>
-            <Select.SelectTrigger
-                id="game"
-                name="game"
-                aria-label="Game"
-                className={`bg-zinc-900 py-3 px-4 rounded text-small flex justify-between text-zinc-500 text-sm`}
-            >
-                <Select.SelectValue placeholder="Selecione o game que deseja jogar" />
-                <Select.SelectIcon>
-                    <CaretDown size={24} className="text-zinc-400" />
-                </Select.SelectIcon>
-            </Select.SelectTrigger>
-            <Select.SelectPortal>
-                <Select.SelectContent className="bg-zinc-900 rounded overflow-hidden">
-                    <Select.SelectScrollUpButton>
-                        <CaretDown size={24} className="text-zinc-400" />
-                    </Select.SelectScrollUpButton>
-                    <Select.SelectViewport className="py-2 px-1">
-                        <Select.SelectGroup>
-                            {props.games.map((game : any) => {
-                            return (
-                                <Select.SelectItem
-                                    key={game.id}
-                                    className="flex items-center justify-between py-2 px-3 m-1 bg-zinc-900 text-zinc-500 cursor-pointer rounded hover:bg-zinc-800 hover:text-white"
-                                    value={game.id}
-                                >
-                                    <Select.SelectItemText>
-                                        {game.title}
-                                    </Select.SelectItemText>
-                                    <Select.SelectItemIndicator>
-                                        <Check size=        {24}                className="text-emerald-500" />
-                                    </Select.SelectItemIndicator>
-                                </Select.SelectItem>
-                            );
-                        })}
-                        </Select.SelectGroup>
-                    </Select.SelectViewport>
-                </Select.SelectContent>
-            </Select.SelectPortal>
-        </Select.Root>
-    )
+
+  const [gamesSelect, setGamesSelect] = useState("")
+
+  return(
+    <Select.Root onValueChange={setGamesSelect}>
+      <Select.SelectTrigger
+        id="game"
+        name="game"
+        aria-label="Game"
+        className={`bg-zinc-900 py-3 px-4 rounded text-small flex justify-between ${
+          gamesSelect ? "text-white" : "text-zinc-500"
+        }`}
+      >
+        <Select.SelectValue placeholder="Selecione o game que deseja jogar" />
+        <Select.SelectIcon>
+          <CaretDown size={24} className="text-zinc-400" />
+        </Select.SelectIcon>
+      </Select.SelectTrigger>
+        <Select.SelectPortal>
+          <Select.SelectContent className="bg-zinc-900 rounded overflow-hidden">
+            <Select.SelectScrollUpButton>
+              <CaretDown size={24} className="text-zinc-400" />
+            </Select.SelectScrollUpButton>
+            <Select.SelectViewport className="py-2 px-1">
+              <Select.SelectGroup>
+                {props.games.map((game : any) => {
+                  return (
+                    <Select.SelectItem
+                      key={game.id}
+                      className="flex items-center justify-between py-2 px-3 m-1 bg-zinc-900 text-zinc-500 cursor-pointer rounded hover:bg-zinc-800 hover:text-white"
+                      value={game.id}
+                    >
+                      <Select.SelectItemText>
+                        {game.title}
+                      </Select.SelectItemText>
+                      <Select.SelectItemIndicator>
+                        <Check size={24} className="text-emerald-500"/>
+                      </Select.SelectItemIndicator>
+                    </Select.SelectItem>
+                  );
+                })}
+              </Select.SelectGroup>
+            </Select.SelectViewport>
+          </Select.SelectContent>
+        </Select.SelectPortal>
+    </Select.Root>
+  )
 }
